d320a375a841a5c2e0c884ee5ffdc4e57bde5054
felipe <felipecalvo6@gmail.com>
2023-09-29 23:45:20 -0300

    entrega1

---
+++ entrega1.py
@@ -0,0 +1,72 @@
+Bdd = {}
+
+registro = (Bdd)
+
+print('Data encendida')
+
+Verdadero = True
+Falso = False
+
+input("Ingresar nombre aqui:")
+
+def Usuario(nombre):
+    nombre = nombre
+    
+    if Usuario: True
+    print( f"el usuario es {self.nombre}")
+
+if not Falso:
+    print("El usuario es incorrecto")
+
+print(Usuario)
+
+input("ingresar contraseña: ")
+def contrasena(password):
+    passwrod="4815162342"
+
+if contrasena: True
+print("la contraseña es correcta")
+
+if not Falso:
+    print("esta no es tu contraseña")
+
+input("ingrese edad: ")
+
+def edad(anos):
+    anos = 26
+
+if True:
+    print("la edad esas correcta")
+
+if not Falso:
+    print("la edad mno es correcta")
+
+ReadData=(Bdd)
+
+guardarArchivo=(Bdd)
+
+login=(Bdd)
+
+nombre=(registro)
+print("hola", (nombre))
+
+def agregarDatos(datos):
+    print(datos)
+    datos.append('otro dato')
+    print('otro dato')
+
+class Humano:
+    def __init__(self, nombre, edad, contrasena):
+        pass
+        self.nombre = nombre
+        self.contrasena = contrasena
+        self.edad = edad
+
+def saludar(self):
+    print(f"Hola soy {self.nombre}, tengo {self.edad} no soy viejo")
+
+humano1 = Humano("epileF", 26, "Efrain234")
+
+humano1.saludar()
+
+print(Bdd)
